02d0d4f0a514ea35dff721f0660105ffa33ea183
Rob Butler <rob@orpheusinteractive.com>
2014-11-18 19:57:54 -0400

    Made player character and a run animation

---
+++ CoolKids/Assets/Scripts/PlayerRunAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRunAnimation : MonoBehaviour {
+
+	[SerializeField] protected Transform rightLeg;
+	[SerializeField] protected Transform leftLeg;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
