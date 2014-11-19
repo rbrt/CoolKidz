3b6d329a7951f6a41da39953e26e7dd42aad379e
Rob Butler <rob@orpheusinteractive.com>
2014-11-19 13:27:25 -0400

    i dunno

--- CoolKids/Assets/Scripts/PlayerShoot.cs
+++ CoolKids/Assets/Scripts/PlayerShoot.cs
@@ -1,18 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerRunAnimation : MonoBehaviour {
-
-	[SerializeField] protected Transform rightLeg;
-	[SerializeField] protected Transform leftLeg;
+public class PlayerShoot : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 }
