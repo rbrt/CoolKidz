12386f8408f4b5a0b65d2ad1df0eb9566a60493b
Rob Butler <rob@vgainteractive.com>
2014-11-20 13:58:49 -0400

    Script and filesystem cleanup

--- CoolKids/Assets/Scripts/PlayerShoot.cs
+++ CoolKids/Assets/Scripts/PlayerShoot.cs
@@ -1,15 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PlayerShoot : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
