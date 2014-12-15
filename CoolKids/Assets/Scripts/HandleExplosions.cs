7796d3c74c25984198af2aaa71fceaa6279b5f8b
Rob Butler <rob@orpheusinteractive.com>
2014-12-15 15:27:59 -0400

    blood fire death

---
+++ CoolKids/Assets/Scripts/HandleExplosions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleExplosions : MonoBehaviour {
+
+	[SerializeField] protected GameObject[] explosions;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(StartExploding());
+	}
+
+	IEnumerator StartExploding(){
+		for (int i = 0; i < explosions.Length; i++){
+			StartCoroutine(HandleExplosion(explosions[i]));
+			yield return new WaitForSeconds(.2f);
+		}
+	}
+
+	IEnumerator HandleExplosion(GameObject explosion){
+		while (true){
+			explosion.SetActive(true);
+			yield return new WaitForSeconds(2);
+			explosion.SetActive(false);
+		}
+	}
+}
